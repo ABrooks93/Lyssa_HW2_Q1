30e7c9faf1a0325047300f617fc98a0e875101a3
ABrooks93 <lyssa@umich.edu>
2025-02-20 01:12:11 -0500

    Completed Q2.a. Has not been tested, subject to change. Good starting point. Additional commits and comments needed.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
 using namespace std;
 
+// 2.a struct created for Check.
+struct Check {
+    int CheckNum;
+    string CheckMemo;
+    double CheckAmount = -1; 
+
+    friend ostream & operator<<(ostream &var, Check &c);
+
+    bool operator>(const Check &c);
+
+}; 
+
+ostream & operator<<(ostream &var, Check &c) {
+    var<<c.CheckNum<<" "<<c.CheckMemo<<" "<<c.CheckAmount;
+    return var;
+}
+
+// bool is returning true or false when check is compared to a float variable.
+bool Check::operator>(Float &f) {
+    return CheckAmount>f;
+}
+
 int main();
