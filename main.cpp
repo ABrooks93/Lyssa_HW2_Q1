9be9cc5040a7cfa2031e79bffbc5de31f8b0e204
ABrooks93 <lyssa@umich.edu>
2025-02-20 23:55:02 -0500

    Part 2 complete

--- main.cpp
+++ main.cpp
@@ -1,26 +1,205 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <random>
+
 using namespace std;
 
-// 2.a struct created for Check.
 struct Check {
     int CheckNum;
     string CheckMemo;
-    double CheckAmount = -1; 
+    double CheckAmount = -1;
 
-    friend ostream & operator<<(ostream &var, Check &c);
+    friend ostream& operator<<(ostream& var, Check& c);
 
-    bool operator>(const Check &c);
+    bool operator>(const Check& c);
+    bool operator>(float f);
+};
 
-}; 
+class CheckBook {
+private:
+    float balance;
+    Check* chkPtr;
+    float lastDeposit;
+    int numOfChecks;
+    int checkBookSize;
 
-ostream & operator<<(ostream &var, Check &c) {
-    var<<c.CheckNum<<" "<<c.CheckMemo<<" "<<c.CheckAmount;
+public:
+    float getBalance() const;
+    float getLastDeposit() const;
+    int getNumOfChecks() const;
+    int getCheckBookSize() const;
+    void setBalance(float bal);
+    void setLastDeposit(float deposit);
+    void setNumOfChecks(int num);
+    void setCheckBookSize(int size);
+
+    CheckBook();
+    CheckBook(float bal);
+    ~CheckBook();
+    CheckBook(const CheckBook& cb);
+    CheckBook& operator=(const CheckBook& cb);
+
+    void deposit(float amount);
+    bool writeCheck(Check& c_amount);
+    void displayChecks() const;
+    void checkTest(CheckBook& cb, float initialBalance);
+};
+
+ostream& operator<<(ostream& var, Check& c) {
+    var << c.CheckNum << " " << c.CheckMemo << " " << c.CheckAmount;
     return var;
 }
 
-// bool is returning true or false when check is compared to a float variable.
-bool Check::operator>(Float &f) {
-    return CheckAmount>f;
+bool Check::operator>(const Check& c) {
+    return CheckAmount > c.CheckAmount;
 }
 
-int main();
+bool Check::operator>(float f) {
+    return CheckAmount > f;
+}
+
+// CheckBook method implementations:
+
+float CheckBook::getBalance() const { return balance; }
+float CheckBook::getLastDeposit() const { return lastDeposit; }
+int CheckBook::getNumOfChecks() const { return numOfChecks; }
+int CheckBook::getCheckBookSize() const { return checkBookSize; }
+void CheckBook::setBalance(float bal) { balance = bal; }
+void CheckBook::setLastDeposit(float deposit) { lastDeposit = deposit; }
+void CheckBook::setNumOfChecks(int num) { numOfChecks = num; }
+void CheckBook::setCheckBookSize(int size) { checkBookSize = size; }
+
+CheckBook::CheckBook() : balance(0.0), lastDeposit(0.0), numOfChecks(0), checkBookSize(4) {
+    chkPtr = new Check[checkBookSize];
+}
+
+CheckBook::CheckBook(float bal) : balance(bal), lastDeposit(0.0), numOfChecks(0), checkBookSize(2) {
+    chkPtr = new Check[checkBookSize];
+}
+
+CheckBook::~CheckBook() {
+    delete[] chkPtr;
+}
+
+CheckBook::CheckBook(const CheckBook& cb) : balance(cb.balance), lastDeposit(cb.lastDeposit), numOfChecks(cb.numOfChecks), checkBookSize(cb.checkBookSize) {
+    chkPtr = new Check[checkBookSize];
+    for (int i = 0; i < numOfChecks; i++) {
+        chkPtr[i] = cb.chkPtr[i];
+    }
+}
+
+CheckBook& CheckBook::operator=(const CheckBook& cb) {
+    if (this != &cb) {
+        delete[] chkPtr;
+        balance = cb.balance;
+        lastDeposit = cb.lastDeposit;
+        numOfChecks = cb.numOfChecks;
+        checkBookSize = cb.checkBookSize;
+        chkPtr = new Check[checkBookSize];
+        for (int i = 0; i < numOfChecks; i++) {
+            chkPtr[i] = cb.chkPtr[i];
+        }
+    }
+    return *this;
+}
+
+void CheckBook::deposit(float amount) { balance += amount; lastDeposit = amount; }
+
+bool CheckBook::writeCheck(Check& c_amount) {
+    if (c_amount > balance) {  // Using overloaded > operator
+        cout << "Insufficient funds.\n";
+        return false;
+    }
+
+    string memos[] = {"wedding", "baby shower", "pizza", "children hospital", "barber shop", "plumber", "hobbit", "trudo"};
+    int numMemos = sizeof(memos) / sizeof(memos[0]);
+
+    cout << "Enter check amount: ";
+    cin >> c_amount.CheckAmount;
+
+    if (c_amount > balance) {
+        cout << "Check amount exceeds remaining balance. Check not processed.\n";
+        return false;
+    }
+
+    cin.ignore(); // Consume the newline character left by cin
+    cout << "Enter check memo (or press Enter for a random memo): ";
+    string userMemo;
+    getline(cin, userMemo);
+
+    if (userMemo.empty()) {
+        int randomIndex = rand() % numMemos; // No srand() here! It's in main()
+        c_amount.CheckMemo = memos[randomIndex];
+        cout<< memos[randomIndex] << endl;
+    } else {
+        c_amount.CheckMemo = userMemo;
+    }
+
+    c_amount.CheckNum = numOfChecks + 1;
+
+    balance -= c_amount.CheckAmount;
+    chkPtr[numOfChecks] = c_amount;
+    numOfChecks++;
+
+    if (numOfChecks >= checkBookSize / 2) {
+        int newSize = checkBookSize * 2;
+        Check* temp = new Check[newSize];
+        for (int i = 0; i < numOfChecks; i++) {
+            temp[i] = chkPtr[i];
+        }
+        delete[] chkPtr;
+        chkPtr = temp;
+        checkBookSize = newSize;
+
+        cout << "WARNING: Checkbook is half full. A new checkbook has been ordered.\n";
+    }
+
+    cout << "Check written successfully.\n";
+    return true;
+}
+
+void CheckBook::displayChecks() const {
+    if (numOfChecks == 0) {
+        cout << "No checks have been written yet.\n";
+        return;
+    }
+
+    for (int i = numOfChecks - 1; i >= 0; i--) {
+        cout << chkPtr[i] << endl; // Using overloaded << operator
+    }
+}
+
+void CheckBook::checkTest(CheckBook& cb, float initialBalance) {
+    cb.setBalance(initialBalance);
+    Check newCheck;
+    int checkNum = 1;
+
+    while (cb.getBalance() > 0) {
+        newCheck.CheckAmount = rand() % 50 + 1;
+        newCheck.CheckNum = checkNum++;
+        if (!cb.writeCheck(newCheck)) {
+            break;
+        }
+        cout << "Balance after check " << newCheck.CheckNum << ": $" << cb.getBalance() << endl;
+    }
+
+    cout << "\nAll checks written (or balance exhausted):\n";
+    cb.displayChecks();
+    cout << "Final checkbook size: " << cb.getCheckBookSize() << endl;
+}
+
+int main() {
+    srand(time(0));
+
+    float initialBalance = 5000.0;
+    CheckBook myCheckBook(initialBalance); // Initialize with balance
+
+    cout << "Starting check test with initial balance: $" << initialBalance << endl;
+    myCheckBook.checkTest(myCheckBook, initialBalance);
+
+    return 0;
+}
